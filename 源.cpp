ffa2cd14d2dca6dd361ce9bda737a64dd4b9334c
雅贤 王 <1125506557@qq.com>
2022-11-26 00:11:10 +0800

    11/26fuck

--- 源.cpp
+++ 源.cpp
@@ -25,58 +25,76 @@ void my_reverse(char bns[])
 }
 int main()
 {
-	char a[100] = { 0 };
-	char b[100] = { 0 };
+	char a[100] = { 0 };				//高精度乘法，思路是乘法竖式计算，每次的结果加起来
+	char b[100] = { 0 };				//倒着存到sum里，最后函数逆序输出
 	scanf("%s %s", a, b);
-	int aw;
-	int bw;
+	int aw;//位数
+	int bw;//
 	int alen = strlen(a);
 	int blen = strlen(b);
 	int i, j = 0;
 	int re;
-	int ris;
-	char sum1[100] = { 0 };
+	int ris = 0;
 	char sum[100] = { 0 };
+	//for (i = 0; i < 100; i++)
+	//{
+	//	sum[i] = '0';
+	//}
 	//高精加法滴
 	int i1;
 	int A1, B1;
 	int alen1;
 	int blen1;
-	int sum0 = 0;
+	int sum0 = 0;//每位相加的和
 	int mid1 = 0;
 	char ans[100] = { 0 };
-	for (bw = blen-1; bw >=0; bw--)
+	for (bw = blen-1; bw >=0; bw--)				//竖式中下面的数的最后一位开始
 	{
-		i = 0 + j;
-		for (aw = alen - 1; aw >= 0; aw--,i++)
-		{
-			ris = 0;
-			re = ((int)a[aw] - 48) * ((int)b[aw] - 48)+ris;
+		//初始化sum1
+		char sum1[100] = { 0 };
+		i = 0 + j;//每次换个数乘时进一位
+		for (aw = alen - 1; aw >= 0; aw--,i++)	//竖式上面的每一位，全部乘完加好加到sum1中，然后最后加到sum中
+		{		
+			re = ((int)a[aw] - 48) * ((int)b[bw] - 48)+ris;//<每两个数相乘的结果>
+			ris = 0;							//
 			if (re > 9)
 			{
-				ris = 1;
-				re -= 10;
-				sum1[i] = (char)(re + 48);
+				ris = re/10;					//ris 上一次乘积大于玖进的位
+				re -= ris*10;					//留下个位
+				sum1[i] = (char)(re + 48);		
 			}
 			else
 			{				
 				sum1[i] = (char)(re + 48);
 			}
-			if (aw == 0 && ris == 1)
+			if (aw == 0 && ris>0)
 			{
-				sum1[i + 1] = '1';
+				sum1[i + 1] = (char)(ris+48);
+				ris = 0;
 			}
 		}
 		//以上为一位乘
 		//下面高精加一下
+
+		
 		alen1 = strlen(sum1);
-		blen1 = strlen(sum);
-		for (i1 = 0; i1 < 100; i1++)
+		blen1 = strlen(sum);				//断点可以直接看两位数相乘的乘法结果
+		for (i1 = j; i1 < 100; i1++)
 		{
 			sum0 = 0;
-			A1 = (int)(sum1[i1] - 48);
+			A1 = (int)(sum1[i1] - 48);		//减的时候自动转啦
 			B1 = (int)(sum[i1] - 48);
-			sum0 = A1 + B1 + mid1;
+			if (sum1[i1] == '\0')
+			{
+				sum0 = B1 + mid1;
+			}
+			else if (sum[i1] == '\0')
+			{
+				sum0 = A1 + mid1;
+			}
+			else
+			sum0 = A1 + B1 + mid1;			//断点看竖式第一行先存了进去
+			mid1 = 0;				//用了再初始化
 			if (sum0 > 9)
 			{
 				mid1 = 1;
@@ -87,15 +105,32 @@ int main()
 			{
 				sum[i1] = (char)(sum0 + 48);
 			}
-			if (i1 > alen1 + 1 && i1 > blen1 + 1)
+			if (i1 >= alen1 - 1 && i1 >= blen1 - 1)
 			{
+				if (sum1[i1+1] != '\0')								//额外一位
+				{
+					sum[i1 + 1] = (char)((int)sum1[i1+1]+mid1);
+					if (sum[i1 + 1] > '9')							//r若超
+					{
+						sum[i1 + 2] = (char)(((int)sum[i1 + 1]-48)/10+48);
+						sum[i1 + 1] = (char)(((int)sum[i1 + 1]-48)%10+48);			//10 %10==0了，要进1的！！！！！！
+						
+						break;
+					}
+				}
+				else if (sum1[i1 + 1] == '\0' && mid1 == 1)
+				{
+					sum[i1 + 1] = '1';
+				}
+				if (sum1[i1 + 2] != '\0')							//额外两位
+				{
+					sum[i1 + 2] = sum1[i1 + 2];
+				}
 				break;
 			}
-			mid1 = 0;
+			
 		}
 		j++;
 	}
-
-
 	my_reverse(sum);
 }
